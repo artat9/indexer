a35da6074399d1ce253c290cf0a626f376194716
artat9 <momodarou2015@gmail.com>
2023-04-11 18:03:18 +0900

    refactor

--- src/indexer_backend/src/lib.rs
+++ src/indexer_backend/src/lib.rs
@@ -1,16 +1,9 @@
 use candid::candid_method;
 use ic_cdk::api::management_canister::http_request::{HttpResponse, TransformArgs};
 use ic_cdk_macros::{query, update};
-use ic_web3::ethabi::Topic;
-use ic_web3::ethabi::{RawLog, TopicFilter};
-use ic_web3::transports::ICHttp;
-use ic_web3::types::FilterBuilder;
-use ic_web3::Web3;
-use ic_web3::{
-    contract::Contract,
-    types::{Address, BlockNumber},
-};
-
+use ic_web3::types::Address;
+use log_finder::{contract, http_client, LogFinder};
+mod log_finder;
 use std::str::FromStr;
 use std::vec;
 
@@ -19,10 +12,6 @@ pub struct TransferEvent {
     recipient: Address,
     amount: u128,
     at: u64,
-}
-
-pub struct Block {
-    events: [TransferEvent],
 }
 
 static mut EVENTS: Vec<TransferEvent> = vec![];
@@ -46,71 +35,33 @@ fn transform(response: TransformArgs) -> HttpResponse {
 #[update(name = "testing")]
 #[candid_method(update)]
 async fn testing() -> Result<String, String> {
-    subscribe(
-        BlockNumber::Number(17022971.into()),
-        BlockNumber::Number(17022973.into()),
-    )
-    .await
+    save_logs(17022971, 17022973).await
 }
 
 const ERC20_ABI: &[u8] = include_bytes!("./abis/erc20.abi");
 const EVENT: &str = "Transfer";
-const WS_ENDPOINT: &str = "https://mainnet.infura.io/v3/TEST";
 const TOKEN_ADDRESS: &str = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH
 
-async fn subscribe(from: BlockNumber, to: BlockNumber) -> Result<String, String> {
-    let web3 = match ICHttp::new(WS_ENDPOINT, None) {
-        Ok(v) => Web3::new(v),
-        Err(e) => return Err(format!("init web3 failed:{}", e)),
-    };
-    let contract = Contract::from_json(
-        web3.eth(),
-        Address::from_str(TOKEN_ADDRESS).unwrap(),
-        ERC20_ABI,
-    )
-    .map_err(|e| format!("init contract failed:{}", e))?;
-    let event_sig = contract.abi().event(EVENT).unwrap().signature();
-    let log_future = web3.eth_filter().create_logs_filter(
-        FilterBuilder::default()
-            .from_block(from)
-            .to_block(to)
-            .address(vec![Address::from_str(TOKEN_ADDRESS).unwrap()])
-            .topic_filter(TopicFilter {
-                topic0: Topic::This(event_sig),
-                topic1: Topic::Any,
-                topic2: Topic::Any,
-                topic3: Topic::Any,
-            })
-            .build(),
-    );
-    let log_result = log_future
-        .await
-        .map_err(|e| format!("create log filter failed:{}", e.to_string()))?;
-    log_result.logs().await.iter().for_each(|logs| {
-        logs.iter().for_each(|log| {
-            let result = contract
-                .abi()
-                .event(EVENT)
+async fn save_logs(from: u64, to: u64) -> Result<String, String> {
+    let web3 = http_client()?;
+    let contract = contract(web3, Address::from_str(TOKEN_ADDRESS).unwrap(), ERC20_ABI)?;
+    let results = LogFinder::new(http_client().unwrap(), contract, EVENT)
+        .find(from, to)
+        .await?;
+    results.into_iter().for_each(|result| {
+        let event = TransferEvent {
+            amount: result.event.params[2]
+                .clone()
+                .value
+                .into_uint()
                 .unwrap()
-                .parse_log(RawLog {
-                    data: log.data.0.clone(),
-                    topics: log.topics.clone(),
-                })
-                .unwrap();
-            let event = TransferEvent {
-                amount: result.params[2]
-                    .clone()
-                    .value
-                    .into_uint()
-                    .unwrap()
-                    .as_u128(),
-                at: log.block_number.unwrap().as_u64(),
-                recipient: result.params[1].clone().value.into_address().unwrap(),
-            };
-            unsafe {
-                EVENTS.push(event);
-            }
-        })
+                .as_u128(),
+            at: result.log.block_number.unwrap().as_u64(),
+            recipient: result.event.params[1].clone().value.into_address().unwrap(),
+        };
+        unsafe {
+            EVENTS.push(event);
+        }
     });
     Ok("ok".to_string())
 }
